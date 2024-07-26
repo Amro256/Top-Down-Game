c2358aa9b51a228493f3505034685c249c14e649
Amro256 <amro.e.sheikh@gmail.com>
2024-07-26 01:12:50 +0100

    Added Mouse rotation

--- Top Down Project/Assets/Scripts/PlayerController.cs
+++ Top Down Project/Assets/Scripts/PlayerController.cs
@@ -10,8 +10,10 @@ public class PlayerController : MonoBehaviour
     public float Speed = 5;
 
     public Rigidbody2D rb;
+    public Camera cam; //refernece to cam
 
-    Vector2 movement; //
+    Vector2 movement; 
+    Vector2 mousePos; //Store Mouse Position
     void Start()
     {
         
@@ -23,11 +25,18 @@ public class PlayerController : MonoBehaviour
         //Trigger/Getting input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate() 
     {     
         //Movement  
-         rb.MovePosition(rb.position + movement * Speed * Time.deltaTime);
+         rb.MovePosition(rb.position + movement.normalized * Speed * Time.deltaTime);
+         
+         //Rotate Player to mouse position
+         Vector2 lookPos = mousePos - rb.position;
+         float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg - 90f;
+         rb.rotation = angle;
     }
 }
